e0f43a471e8f19b6a4e27fd33dad2638d7a4c4ee
gaoxuefeng <2392627525@qq.com>
2020-12-24 15:38:54 +0800

    .

---
+++ src/router/routes/Vip/Vip.js
@@ -0,0 +1,8 @@
+import Viplist from "@/views/Viplist/Viplist";
+var vipRouter =[
+    {
+        path:"/viplist",
+        component:Viplist
+    },
+]
+export default vipRouter
---
+++ src/views/Viplist/Viplist.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <table class="table table-bordered">
+            <tr>
+                <td>fghfd1</td>
+                <td></td>
+                <td></td>
+                </tr>
+ </table>
+    </div>
+</template>
+<script>
+// import "@/assets/bootstarp-3.3.7/dist/css/bootstarp.min.css"
+export default {
+    
+}
+</script>
